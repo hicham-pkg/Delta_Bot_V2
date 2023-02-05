1b6aff12a5812427c827f85794c669e119dd2375
Hicham <hichamtabbakh@hotmail.com>
2023-02-05 17:33:26 +0100

    New feature

---
+++ plugins/searcher-google.js
@@ -0,0 +1,19 @@
+import { googleIt } from '@bochilteam/scraper'
+let handler = async (m, { conn, command, args }) => {
+const fetch = (await import('node-fetch')).default
+let full = /f$/i.test(command)
+let text = args.join` `
+if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] أدخل النص أو الموضوع الذي تريد البحث عنه*', m)
+let url = 'https://google.com/search?q=' + encodeURIComponent(text)
+let search = await googleIt(text)
+let msg = search.articles.map(({ title, url, description }) => { return `*${title}*\n_${url}_\n_${description}_` }).join('\n\n')
+try {
+let ss = await (await fetch(`https://api.lolhuman.xyz/api/ssweb?apikey=${lolkeysapi}&url=${url}`)).arrayBuffer()
+await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
+} catch {
+m.reply(msg)
+}}
+handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
+handler.tags = ['internet']
+handler.command = /^googlef?$/i
+export default handler
