653657e35d39dd56947877685ddffa0c9a610bb9
Hicham <hichamtabbakh@hotmail.com>
2023-02-08 21:22:50 +0100

    Delta-Bot

--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, text }) => {
-let who
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-else who = m.chat
-if (!who) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝙶𝚁𝙴𝚂𝙰 𝙴𝙻 @𝚝𝚊𝚐 𝙳𝙴 𝙻𝙰 𝙿𝙴𝚁𝚂𝙾𝙽𝙰 𝚀𝚄𝙴 𝙳𝙴𝚂𝙴𝙴 𝙰𝙶𝚁𝙴𝙶𝙰𝚁 𝙰 𝙻𝙾𝚂 𝚄𝚂𝚄𝙰𝚁𝙸𝙾𝚂 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*`
-if (global.prems.includes(who.split`@`[0])) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝙶𝚁𝙴𝚂𝙰𝙳𝙾 𝚈𝙰 𝙴𝚂 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼*'
-global.prems.push(`${who.split`@`[0]}`)
-let textprem = `*[❗𝐈𝐍𝐅𝐎❗] @${who.split`@`[0]} 𝙰𝙷𝙾𝚁𝙰 𝙴𝚂 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙿𝚁𝙴𝙼𝙸𝚄𝙼, 𝙽𝙾 𝚃𝙴𝙽𝙳𝚁𝙰 𝙻𝙸𝙼𝙸𝚃𝙴𝚂 𝙰𝙻 𝚄𝚂𝙰𝚁 𝙴𝙻 𝙱𝙾𝚃*`
-m.reply(textprem, null, { mentions: conn.parseMention(textprem) })
-}
-handler.help = ['addprem <@user>']
-handler.tags = ['owner']
-handler.command = /^(add|\+)prem$/i
-handler.group = true
-handler.rowner = true
-export default handler
+    let who
+    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+    else who = m.chat
+    if (!who) throw `*[❗𝐈𝐍𝐅𝐎❗] أدخل علامة @ الخاصة بالشخص الذي تريد إضافته إلى المستخدمين المميزين*`
+    if (global.prems.includes(who.split`@`[0])) throw '*[❗𝐈𝐍𝐅𝐎❗] المستخدم الذي قام بتسجيل الدخول هو بالفعل مستخدم متميز*'
+    global.prems.push(`${who.split`@`[0]}`)
+    let textprem = `*[❗𝐈𝐍𝐅𝐎❗] @${who.split`@`[0]} أنت الآن مستخدم متميز ، ولن يكون لديك حدود عند استخدام الروبوت*`
+    m.reply(textprem, null, { mentions: conn.parseMention(textprem) })
+    }
+    handler.help = ['addprem <@user>']
+    handler.tags = ['owner']
+    handler.command = /^(add|\+)prem$/i
+    handler.group = true
+    handler.rowner = true
+    export default handler
