a75a98cf73d31ef4d3c54f68a0f4092d5b39f4ea
Hicham <hichamtabbakh@hotmail.com>
2023-02-17 22:59:10 +0100

    Delta Bot

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -5,7 +5,7 @@ if (global.conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 conn.reply(m.chat, stdout.toString(), m)}
 } catch {
-var update = execSync('git config --global user.email "hichamtabbakh@hotmail.com" && git config --global user.name "hicham-pkg" && git pull origin main')
+    var update = execSync('git remote set-url origin https://github.com/hicham-pkg/Delta_Bot_V1 && git pull')
 await m.reply(update.toString())
 }}
 handler.help = ['update']
