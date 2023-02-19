cdad367798e79f174d1c3a3f80ad85ca4b310ed4
Hicham <hichamtabbakh@hotmail.com>
2023-02-19 20:33:30 +0100

    Delta Bot

--- plugins/_simsimi.js
+++ plugins/_simsimi.js
@@ -5,7 +5,7 @@ handler.before = async (m) => {
 let chat = global.db.data.chats[m.chat]
 if (chat.simi) {
 if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
-let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=es`)
+let ressimi = await fetch(`https://api.simsimi.net/v2/?text=${encodeURIComponent(m.text)}&lc=en`)
 let data = await ressimi.json();
 //if (data.success == 'I don't know what you're saying. Please teach me.') await m.reply("😵‍💫")                                                                                                 
 await m.reply(data.success)
