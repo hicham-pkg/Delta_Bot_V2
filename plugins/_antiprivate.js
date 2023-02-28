783a29529dcc9ea4f07f32c5e5fc295c2c61deac
Hicham <hichamtabbakh@hotmail.com>
2023-02-28 12:37:42 +0100

    Delta Bot

--- plugins/_antiprivate.js
+++ plugins/_antiprivate.js
@@ -6,7 +6,7 @@ export async function before(m, { conn, isAdmin, isBotAdmin, isOwner, isROwner }
     let chat = global.db.data.chats[m.chat]
     let bot = global.db.data.settings[this.user.jid] || {}
     if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, It is forbidden to speak to the private of the bot for which you will be blocked.*`, false, { mentions: [m.sender] })
+    await m.reply(`*[❗] Hello @${m.sender.split`@`[0]}, It is forbidden to speak to the private of the bot for which you will be blocked.*`, false, { mentions: [m.sender] })
     await this.updateBlockStatus(m.chat, 'block')}
     return !1
     }
--- plugins/owner-addprem.js
+++ plugins/owner-addprem.js
@@ -1,16 +1,17 @@
 let handler = async (m, { conn, text }) => {
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-    else who = m.chat
-    if (!who) throw `*[❗𝐈𝐍𝐅𝐎❗] أدخل علامة @ الخاصة بالشخص الذي تريد إضافته إلى المستخدمين المميزين*`
-    if (global.prems.includes(who.split`@`[0])) throw '*[❗𝐈𝐍𝐅𝐎❗] المستخدم الذي قام بتسجيل الدخول هو بالفعل مستخدم متميز*'
-    global.prems.push(`${who.split`@`[0]}`)
-    let textprem = `*[❗𝐈𝐍𝐅𝐎❗] @${who.split`@`[0]} أنت الآن مستخدم متميز ، ولن يكون لديك حدود عند استخدام الروبوت*`
-    m.reply(textprem, null, { mentions: conn.parseMention(textprem) })
-    }
-    handler.help = ['addprem <@user>']
-    handler.tags = ['owner']
-    handler.command = /^(add|\+)prem$/i
-    handler.group = true
-    handler.rowner = true
-    export default handler
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
+else who = m.chat
+if (!who) throw `*[❗𝐈𝐍𝐅𝐎❗] أدخل علامة @ الخاصة بالشخص الذي تريد إضافته إلى المستخدمين المميزين*`
+let user = global.db.data.users[who]
+//if (user.premium = true) throw '*[❗𝐈𝐍𝐅𝐎❗] المستخدم الذي تم إدخاله هو بالفعل مستخدم متميز*'
+user.premium = true
+let textprem = `*[❗𝐈𝐍𝐅𝐎❗] @${who.split`@`[0]} أنت الآن مستخدم متميز ، ولن يكون لديك حدود عند استخدام الروبوت*`
+m.reply(textprem, null, { mentions: conn.parseMention(textprem) })
+}
+handler.help = ['addprem <@user>']
+handler.tags = ['owner']
+handler.command = /^(add|\+)prem$/i
+handler.group = true
+handler.rowner = true
+export default handler
