94e59d4b597e497187ceab1f1a2447f73e646f93
Hicham <hichamtabbakh@hotmail.com>
2023-03-08 12:53:46 +0100

    Delta Bot

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -1,20 +1,18 @@
-import fetch from 'node-fetch'
-
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `[❗𝐈𝐍𝐅𝐎❗] enter the name of an instagram user*\n\n*Exemple:*\n*${usedPrefix + command} joe_hattab`
-  await m.reply(global.wait)
-  const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=your_lolhuman_api_key`)
+  if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] أدخل اسم مستخدم instagram*\n\n*مثال:*\n*${usedPrefix + command} cristiano
+  *`
+  await m.reply(global.wait)    
+  const res = await fetch(`https://api.lolhuman.xyz/api/igstory/${args[0]}?apikey=${lolkeysapi}`)
   var anu = await res.json()
   var anuku = anu.result
-  if (anuku == '') return m.reply('*[❗] invalid user or no stories*')
+  if (anuku == '') return m.reply('*[❗] Invalid user or no stories*')  
   for (var i of anuku) {
-    let res = await axios.head(i)
-    let mime = res.headers['content-type']
-    if (/image/.test(mime)) await conn.sendFile(m.chat, i, 'error.jpg', null, m).catch(() => { return m.reply('*[❗] invalid user or no stories*')})
-    if (/video/.test(mime)) await conn.sendFile(m.chat, i, 'error.mp4', null, m).catch(() => { return m.reply('*[❗] invalid user or no stories*')})
-  }
-}
-handler.help = ['igstory <username>']
-handler.tags = ['downloader']
-handler.command = ['instastory', 'igs']
-export default handler
+  let res = await axios.head(i)
+  let mime = res.headers['content-type']
+  if (/image/.test(mime)) await conn.sendFile(m.chat, i, 'error.jpg', null, m).catch(() => { return m.reply('*[❗] Invalid user or no stories*')})
+  if (/video/.test(mime)) await conn.sendFile(m.chat, i, 'error.mp4', null, m).catch(() => { return m.reply('*[❗] Invalid user or no stories*')})
+  }}
+  handler.help = ['igstory <username>']
+  handler.tags = ['downloader']
+  handler.command = ['igstory', 'instastory' ]
+  export default handler
