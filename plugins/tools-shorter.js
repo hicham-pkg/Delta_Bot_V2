f61e4727de8b3e34d40b1393a69323b3819dabb8
Hicham <hichamtabbakh@hotmail.com>
2023-03-13 17:02:48 +0100

    Delta Bot

--- plugins/tools-shorter.js
+++ plugins/tools-shorter.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async(m, { conn, args, text }) => {
 if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] Enter a link / url which you want to shorten*'
-let shortUrl1 = await (await fetch(`https://api.lolhuman.xyz/api/shortlink3?apikey=BrunoSobrino&url=${args[0]}`)).text()  
+let shortUrl1 = await (await fetch(`https://api.lolhuman.xyz/api/shortlink3?apikey=${lolkeysapi}&url=${args[0]}`)).json()
 if (!shortUrl1) throw `*[❗] Error, check that the text entered is a text and try again*`
 let done = `*Link shortened correctly!!*\n\n*previous link:*\n${text}\n*shortened link:*\n${shortUrl1}`.trim()   
 m.reply(done)}
