710009abe0a931601a1156b486397b7ceda325c7
Hicham <hichamtabbakh@hotmail.com>
2023-03-13 17:06:48 +0100

    Delta Bot

--- plugins/tools-shorter.js
+++ plugins/tools-shorter.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, args, text }) => {
 if (!text) throw '*[❗𝐈𝐍𝐅𝐎❗] Enter a link / url which you want to shorten*'
 let shortUrl1 = await (await fetch(`https://api.lolhuman.xyz/api/shortlink3?apikey=BrunoSobrino&url=${args[0]}`)).json()
 if (shortUrl1.error) throw `*[❗] Error, check that the text entered is a text and try again*`
-let shortenedLink = shortUrl1.result.link
+let shortenedLink = shortUrl1.result.split('\n')[0]
 let done = `*Link shortened correctly!!*\n\n*previous link:*\n${text}\n*shortened link:*\n${shortenedLink}`.trim()
  
 m.reply(done)}
